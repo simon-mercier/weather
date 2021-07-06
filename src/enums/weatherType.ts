c366ff85b7123350525ac243cb61f7cb561ed396
simonMerci <nomismercier@gmail.com>
2021-07-05 22:46:17 -0400

    Added an enum of possible weather types

---
+++ src/enums/weatherType.ts
@@ -0,0 +1,14 @@
+
+export enum WeatherType {
+    SUNNY,
+    PARTIALLY_CLOUDY,
+    CLOUDY,
+    OVERCAST,
+    RAIN,
+    DRIZZLE,
+    SNOW,
+    TUNDERSTORM,
+    FOG,
+    HURRICANE,
+    SANDSTORM,
+}

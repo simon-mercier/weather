8cb23c3085c1b37116add78129eadeae8b261a4f
simonMerci <nomismercier@gmail.com>
2021-08-02 22:08:16 -0400

    Removed old weather icons

--- src/components/current-weather/weather-icons/Tunderstorm.tsx
+++
@@ -1,9 +0,0 @@
-import Cloud1 from "../../../assets/icons/cloud1.svg";
-import Cloud2 from "../../../assets/icons/cloud2.svg";
-import Sun from "../../../assets/icons/sun.svg";
-import "./WeatherIconsStyles.scss";
-const Tunderstorm = () => {
-    return <div></div>;
-};
-
-export default Tunderstorm;
--- src/contexts/LocationCoordinates.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext, Dispatch, SetStateAction } from "react";
-import ICoordinates from "../interfaces/coordinates";
-
-const LocationCoordinatesContext = createContext<
-    Dispatch<SetStateAction<ICoordinates>>
->(() => null);
-
-export default LocationCoordinatesContext;

10aa2e19f50dde0ac91ba0952d0127944c61e6c8
simon-mercier <simon-merci@outlook.com>
2021-10-03 10:16:37 -0400

    Adde build folder to client

--- server/app.js
+++ server/app.js
@@ -20,10 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use("/api", apiRouter);

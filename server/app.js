b2f1e9490fa6eef6052163215b7b162b81752065
simon-mercier <simon-merci@outlook.com>
2021-10-03 10:00:30 -0400

    Added current build to the server

--- server/app.js
+++ server/app.js
@@ -20,6 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler

5fb5907b01473afffb8f8c71c019e573b2f9e358
simon-mercier <simon-merci@outlook.com>
2021-10-15 10:27:57 -0400

    redirected build to serverside

--- client/src/components/icons/WeatherIcon.tsx
+++ client/src/components/icons/WeatherIcon.tsx
@@ -2,17 +2,37 @@ import styled from "styled-components";
 import WeatherType from "../../enums/weatherType";
 
 import PngClear from "../../assets/icons/png/clear.png";
+import PngClearNight from "../../assets/icons/png/clear-night.png";
+
 import PngPartlyCloudy from "../../assets/icons/png/partly-cloudy.png";
+import PngPartlyCloudyNight from "../../assets/icons/png/partly-cloudy-night.png";
+
 import PngMostlyCloudy from "../../assets/icons/png/mostly-cloudy.png";
 import PngCloudy from "../../assets/icons/png/cloudy.png";
 import PngRain from "../../assets/icons/png/rain.png";
 import PngMist from "../../assets/icons/png/mist.png";
 import PngSnow from "../../assets/icons/png/snow.png";
 import PngThunder from "../../assets/icons/png/thunder.png";
+import PeriodsOfDay from "../../enums/periodsOfDay";
+import { useContext } from "react";
+import PeriodOfDayContext from "../../contexts/PeriodOfDay";
 
-const weatherType2IconPNG = new Map<WeatherType, any>([
+const weatherType2IconDay = new Map<WeatherType, any>([
     [WeatherType.CLEAR_SKY, PngClear],
     [WeatherType.FEW_CLOUDS, PngPartlyCloudy],
+    [WeatherType.SCATTERED_CLOUDS, PngPartlyCloudy],
+    [WeatherType.BROKEN_CLOUDS, PngMostlyCloudy],
+    [WeatherType.OVERCAST_CLOUDS, PngCloudy],
+    [WeatherType.DRIZZLE, PngRain],
+    [WeatherType.RAIN, PngRain],
+    [WeatherType.SMOKE, PngMist],
+    [WeatherType.SNOW, PngSnow],
+    [WeatherType.THUNDERSTORM, PngThunder],
+]);
+
+const weatherType2IconNight = new Map<WeatherType, any>([
+    [WeatherType.CLEAR_SKY, PngClearNight],
+    [WeatherType.FEW_CLOUDS, PngPartlyCloudyNight],
     [WeatherType.SCATTERED_CLOUDS, PngPartlyCloudy],
     [WeatherType.BROKEN_CLOUDS, PngMostlyCloudy],
     [WeatherType.OVERCAST_CLOUDS, PngCloudy],
@@ -31,9 +51,17 @@ interface WeatherIconProps {
 }
 
 const WeatherIcon = ({ width, height, weatherType }: WeatherIconProps) => {
+    const periodOfDay: PeriodsOfDay = useContext(PeriodOfDayContext);
+    console.log(periodOfDay);
     return (
         <Container width={width} height={height}>
-            <IconPng src={weatherType2IconPNG.get(weatherType)} />
+            <IconPng
+                src={
+                    periodOfDay == PeriodsOfDay.NIGHT
+                        ? weatherType2IconNight.get(weatherType)
+                        : weatherType2IconDay.get(weatherType)
+                }
+            />
         </Container>
     );
 };
